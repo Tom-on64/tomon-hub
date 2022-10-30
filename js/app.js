017502d0bda35c5fe0a589d5e7e33e8006389829
Tom-on64 <kubecekt@gmail.com>
2022-10-30 11:21:11 +0100

    Fixed Formatting

--- js/app.js
+++ js/app.js
@@ -1,36 +1,35 @@
 // DOM
 
-const darkButton = document.getElementById("dark")
-const lightButton = document.getElementById("light")
-const solarButton = document.getElementById("solar")
-const body = document.body
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+const solarButton = document.getElementById("solar");
+const body = document.body;
 
-const theme = localStorage.getItem('theme');
-const isSolar = localStorage.getItem('isSolar');
+const theme = localStorage.getItem("theme");
+const isSolar = localStorage.getItem("isSolar");
 
 if (theme) {
   body.classList.add(theme);
-  isSolar && body.classList.add('solar');
+  isSolar && body.classList.add("solar");
 }
 
 // Button Events
 darkButton.onclick = () => {
-    body.classList.replace("light", "dark")
-    localStorage.setItem('theme', 'dark');
-}
+  body.classList.replace("light", "dark");
+  localStorage.setItem("theme", "dark");
+};
 lightButton.onclick = () => {
-    body.classList.replace("dark", "light")
-    localStorage.setItem('theme', 'light');
-}
+  body.classList.replace("dark", "light");
+  localStorage.setItem("theme", "light");
+};
 solarButton.onclick = () => {
-    if (body.classList.contains("solar")) {
-        body.classList.remove("solar")
-        solarButton.textContent = "Solarise"
-        localStorage.removeItem('isSolar');
-    }
-    else {
-        body.classList.add("solar")
-        solarButton.textContent = "Normalise"
-        localStorage.setItem('isSolar', true);
-    }
-}
+  if (body.classList.contains("solar")) {
+    body.classList.remove("solar");
+    solarButton.textContent = "Solarise";
+    localStorage.removeItem("isSolar");
+  } else {
+    body.classList.add("solar");
+    solarButton.textContent = "Normalise";
+    localStorage.setItem("isSolar", true);
+  }
+};
